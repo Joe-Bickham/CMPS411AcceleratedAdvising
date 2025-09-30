ae471371641560ea0c7a8e506b6009a925b826ed
Gdotpierre <98052272+Gdotpierre@users.noreply.github.com>
2025-09-30 18:21:32 -0500

    Minor changes

--- PdfScraper/obj/Debug/net9.0/PdfScraper.AssemblyInfo.cs
+++ PdfScraper/obj/Debug/net9.0/PdfScraper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PdfScraper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a50d89fed0cfb6f3538c0c9cb8452ad323468c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc94431a8481191a19d63d05d962222c48ec8aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PdfScraper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PdfScraper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
