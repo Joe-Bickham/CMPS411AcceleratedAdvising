5776acc0aa2dd3f47d81f3b9b27be617bd98b77f
JoeBickham <joe3bickham@gmail.com>
2025-11-11 17:57:38 -0600

    Update Claude API key to Acc-Advisor

--- PdfScraper/obj/Debug/net9.0/PdfScraper.AssemblyInfo.cs
+++ PdfScraper/obj/Debug/net9.0/PdfScraper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PdfScraper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c91aedc45d584bed607d2956e26b84f192f0e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613a9663f203f1b2f76a437bb8d005a257ad74e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PdfScraper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PdfScraper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
